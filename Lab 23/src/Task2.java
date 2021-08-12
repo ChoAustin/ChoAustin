f884ddd0e0d4b864007b84520cc3eb7a9924c772
ChoAustin <austincho1997@gmail.com>
2021-08-12 17:51:26 -0400

    Lab 23

---
+++ Lab 23/src/Task2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Task2 
+{
+	public static void main (String[] args)
+	{
+		int[] table1 = new int[4];
+		int[] table2 = new int[4];
+		
+		for (int row = 0; row < table1.length; row++)
+			System.out.println(table1);
+		
+
+	}	
+}
