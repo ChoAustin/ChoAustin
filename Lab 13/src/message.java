23a40228e6c44a53e6bdaf6303c700856b00cf95
ChoAustin <austincho1997@gmail.com>
2021-08-12 17:50:16 -0400

    Lab 13

---
+++ Lab 13/src/message.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class message 
+{
+
+	public static void main(String[] args)
+	{
+		double prints = 1;
+		double count = 0; 
+		Scanner scan = new Scanner(System.in);
+		while (count <= prints) 
+		
+			
+		{
+			
+			System.out.println("How many prints?: ");
+			prints = scan.nextDouble();
+			 
+			System.out.println("Computer Science is cool!");
+			count++;
+			
+		}
+	}
+}
